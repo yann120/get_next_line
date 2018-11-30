6cd310b48d191558d427d7568d66a14920a1604a
Yann PETITJEAN <ypetitje@e1r8p5.42.fr>
2018-11-30 20:14:28 +0100

    working with one line

--- get_next_line.c
+++ get_next_line.c
@@ -10,22 +10,70 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// 10420
+
 #include "get_next_line.h"
+#include "../../libft/verif/libft.h"
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFF_SIZE 20000
+#define BUFF_SIZE 5
+
+int get_next_line(const int fd, char **line)
+{
+	char	buf[BUFF_SIZE + 1];
+	// char	**headtemp;
+	// char	*head;
+	// int idebug = 0;
+	static char *rest;
+	char result[10420];
+	int i = 0;
+	int j;
+
+
+	// temp = 
+	while (read(fd, &buf, BUFF_SIZE) != 0)
+	{	
+		j = 0;
+		buf[BUFF_SIZE] = '\0';
+		// ft_putstr(buf);
+		// printf("\n");
+		if (ft_strchr(buf, '\n'))
+		{
+			while (buf[j] != '\n')
+				result[i++] = buf[j++];
+			result[i] = '\0';
+			*line = result;
+			rest = ft_strsub(buf, j, ft_strlen(buf) - j);
+			return (1);
+			// printf("new line%d\n", idebug++);
+		}
+		else
+		{
+			// printf("new string\n");
+			while (buf[j])
+				result[i++] = buf[j++];
+		}
+			// write(1, &buf, BUFF_SIZE);
+	}
+	return (0);
+}
 
 int		main(void)
 {
 	int		fd;
-	char	buf[BUFF_SIZE + 1];
+	char	*line;
 
-	fd = open("../st_exupery_le_petit_prince.txt", O_RDONLY);
-	while (read(fd, &buf, BUFF_SIZE) != 0)
-	{	
-		if (strchr(buf, '\n'))
-		write(1, &buf, BUFF_SIZE);
-	}
+	fd = open("../poeme.txt", O_RDONLY);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 	return (0);
 }
