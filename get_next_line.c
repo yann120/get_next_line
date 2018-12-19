8366f8552db079b2d7f5bbd528e6284c170df41c
Yann PETITJEAN <ypetitje@e1r7p6.42.fr>
2018-12-19 19:07:37 +0100

    gnl working with basic test

--- get_next_line.c
+++ get_next_line.c
@@ -1,65 +1,88 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line3.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yann <yann@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/17 09:29:34 by yann              #+#    #+#             */
-/*   Updated: 2018/12/17 15:56:33 by yann             ###   ########.fr       */
+/*   Created: 2018/12/18 15:25:38 by yann              #+#    #+#             */
+/*   Updated: 2018/12/18 16:25:53 by yann             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_new_line(char **s, char **line, int fd, int ret)
+int	return_good_line(char *result, int *position, char **line)
 {
-	char 	*tmp;
-	int 	len;
+	int start;
 
-	len = 0;
-	while (s[0][len] != '\n' && s[0][len] != '\0')
-		len++;
-	if (s[0][len] == '\n')
+	start = *position;
+	// printf("start %d\n", start);
+	while (result[*position] != '\0')
 	{
-		*line = ft_strsub(s[0], 0, len);
-		tmp = ft_strdup(s[0] + len + 1);
-		free(s[0]);
-		s[0] = tmp;
-		if (s[0][0] == '\0')
-			ft_strdel(&s[0]);
+		if (result[*position] == '\n')
+		{
+			*line = ft_strsub(result, start, *position - start);
+			// printf("%s\n", *line);
+			(*position)++;
+			return(1);	
+		}
+		(*position)++;
+		// printf("position %d\n", *position);
 	}
-	else if (s[0][len] == '\0')
+	if (result[*position] == '\0')
 	{
-		*line = ft_strdup(s[0]);
-		ft_strdel(&s[0]);
+		*line = ft_strsub(result, start, *position - start);
+		// *position = -123;
 	}
-	return (1);
+	if (start < *position - BUFF_SIZE)
+	{
+		*position = -123;
+		return(1);
+	}
+	// else
+		*position = -123;
+		return(0);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static 	char *s[1];
-	int		ret;
-	char	*tmp;
-	char	buf[BUFF_SIZE + 1];
+	static	char		*result;
+	char				buf[BUFF_SIZE + 1];
+	char				*temp;
+	int					returnvalue;
+	static	int			position;
 
-	if (fd < 0 || line == NULL )
-		return (-1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		buf[ret] = '\0';
-		if (s[0] == NULL)
-			s[0] = ft_strnew(1);
-		tmp = ft_strjoin(s[0], buf);
-		free(s[0]);
-		s[0] = tmp;
-		if (ft_strchr(buf, '\n'))
-			break ;
+	//copie l'integralite dans result
+	while ((returnvalue = read(fd, buf, BUFF_SIZE)) > 0)
+	{	
+		position = 0;
+		buf[returnvalue]  = '\0';
+		if (result == NULL)
+			result = ft_strnew(1);
+		temp = ft_strjoin(result, buf);
+		ft_strdel(&result);
+		result = temp;
 	}
-	if (ret < 0)
+	if (returnvalue < 0)
 		return (-1);
-	else if (ret == 0 && (s[0] == NULL || s[0][0] == '\0'))
+	else if (returnvalue == 0 && position == -123)
 		return (0);
-	return(ft_new_line(s, line, fd, ret));
+	return(return_good_line(result, &position, line)); 
 }
+
+// int main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+// 	int		i = 0;
+
+// 	// fd = open("../poeme.txt", O_RDONLY);
+// 	fd = open("gnl-sample/m83.txt", O_RDONLY);
+// 	while(i++ < 9)
+// 	{
+// 		get_next_line(fd, &line);
+// 		printf("%s\n", line);
+// 	}
+// 	return 0;
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -12,12 +12,14 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 80
+# define BUFF_SIZE 1
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
-# include "libft.h"
+# include "libft/includes/libft.h"
+#include <unistd.h>
+#include <fcntl.h>
 
 int get_next_line(const int fd, char **line);
+int	return_good_line(char *result, int *position, char **line);
 
 #endif
