7c9c8260e9e54f14aaf261b45814db0d0f62c6c1
Yann PETITJEAN <ypetitje@e1r7p4.42.fr>
2018-12-20 17:11:07 +0100

    now working on basic test with every buffsize

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line3.c                                   :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yann <yann@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ypetitje <ypetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 15:25:38 by yann              #+#    #+#             */
-/*   Updated: 2018/12/18 16:25:53 by yann             ###   ########.fr       */
+/*   Updated: 2018/12/20 17:09:10 by ypetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// quand buffer superieur a line, retourne pas la bonne valeur
 
 #include "get_next_line.h"
 
@@ -31,18 +33,14 @@ int	return_good_line(char *result, int *position, char **line)
 		// printf("position %d\n", *position);
 	}
 	if (result[*position] == '\0')
-	{
 		*line = ft_strsub(result, start, *position - start);
-		// *position = -123;
-	}
-	if (start < *position - BUFF_SIZE)
+	if (start < *position)
 	{
 		*position = -123;
 		return(1);
 	}
-	// else
-		*position = -123;
-		return(0);
+	*position = -123;
+	return(0);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -51,23 +49,26 @@ int		get_next_line(const int fd, char **line)
 	char				buf[BUFF_SIZE + 1];
 	char				*temp;
 	int					returnvalue;
-	static	int			position;
+	static	int			position = 0;
 
 	//copie l'integralite dans result
 	while ((returnvalue = read(fd, buf, BUFF_SIZE)) > 0)
 	{	
-		position = 0;
+		
 		buf[returnvalue]  = '\0';
 		if (result == NULL)
 			result = ft_strnew(1);
 		temp = ft_strjoin(result, buf);
 		ft_strdel(&result);
 		result = temp;
+		if (ft_strchr(buf, '\n'))
+			return(return_good_line(result, &position, line));
 	}
 	if (returnvalue < 0)
 		return (-1);
 	else if (returnvalue == 0 && position == -123)
 		return (0);
+	// return(0); 
 	return(return_good_line(result, &position, line)); 
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yann <yann@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ypetitje <ypetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 09:50:09 by ypetitje          #+#    #+#             */
-/*   Updated: 2018/12/17 14:29:14 by yann             ###   ########.fr       */
+/*   Updated: 2018/12/20 17:09:54 by ypetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 1000
 # include <stdio.h>
 # include <stdlib.h>
 # include "libft/includes/libft.h"
