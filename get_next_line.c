49be0fa13a43445b65874eb04030b17ad3e98f76
Yann PETITJEAN <ypetitje@e1r7p4.42.fr>
2019-01-05 18:34:17 +0100

    working with multiple fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ypetitje <ypetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 15:25:38 by yann              #+#    #+#             */
-/*   Updated: 2019/01/05 18:03:49 by ypetitje         ###   ########.fr       */
+/*   Updated: 2019/01/05 18:32:12 by ypetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ int	ft_read(char **result, int fd)
 	if ((returnvalue = read(fd, buf, BUFF_SIZE)) < 0)
 		return (-1);
 	buf[returnvalue] = 0;
-	temp = *result;
-	*result = ft_strjoin(*result, buf);
+	temp = result[fd];
+	result[fd] = ft_strjoin(result[fd], buf);
 	free(temp);
 	return (returnvalue);
 }
 
-int ft_return_good_line(char **result, char *temp, char **line)
+int ft_return_good_line(char **result, char *temp, char **line, int fd)
 {
 	int returnvalue;
 	char *tofree;
@@ -37,16 +37,16 @@ int ft_return_good_line(char **result, char *temp, char **line)
 	else
 		returnvalue = 0;
 	*temp = 0;
-	*line = ft_strdup(*result);
-	if (returnvalue == 0 && ft_strlen(*result) > 0)
+	*line = ft_strdup(result[fd]);
+	if (returnvalue == 0 && ft_strlen(result[fd]) > 0)
 	{
-		*result = ft_strnew(1);
+		result[fd] = ft_strnew(1);
 		return (1);
 	}
-	else if (returnvalue == 0 && !(ft_strlen(*result)))
+	else if (returnvalue == 0 && !(ft_strlen(result[fd])))
 		return (0);
-	tofree = *result;
-	*result = ft_strdup(temp + 1);
+	tofree = result[fd];
+	result[fd] = ft_strdup(temp + 1);
 	free(tofree);
 	return (returnvalue);
 }
@@ -54,24 +54,24 @@ int ft_return_good_line(char **result, char *temp, char **line)
 int get_next_line(int const fd, char **line)
 {
 	int				returnvalue;
-	static char 	*result;
+	static char 	*result[1000];
 	char			*temp;
 
-	if (line == NULL || BUFF_SIZE < 1)
+	if (fd < 0 || line == NULL || BUFF_SIZE < 1)
 		return (-1);
-	if (result == NULL)
-		result = ft_strnew(1);
+	if (result[fd] == NULL)
+		result[fd] = ft_strnew(1);
 	returnvalue = BUFF_SIZE;
 	while (line != NULL)
 	{
-		temp = result;
+		temp = result[fd];
 		while (*temp != 0 || returnvalue < BUFF_SIZE)
 		{
 			if (*temp == '\n' || *temp == '\0')
-				return (ft_return_good_line(&result, temp, line));
+				return (ft_return_good_line(result, temp, line, fd));
 			temp++;
 		}
-		if ((returnvalue = ft_read(&result, fd)) < 0)
+		if ((returnvalue = ft_read(result, fd)) < 0)
 			return (-1);
 	}
 	return (0);
